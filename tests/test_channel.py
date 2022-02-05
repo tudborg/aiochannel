769b52151ec24a8daeedaae03df7ba62e6ad19be
Henrik Tudborg <henrik@tudb.org>
2022-02-05 22:01:26 +0100

    Adding python 3.10 support
    Converting the test cases to use aiounittest.
    There is a lot to be done around cleaning up tests though.

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -1,38 +1,25 @@
-from .case import TestCase
+import aiounittest
 import asyncio
 from aiochannel import Channel, ChannelClosed, ChannelFull, ChannelEmpty
 
 
-class ChannelTest(TestCase):
-
-    def arun(self, coros_or_future):
-        return self.loop.run_until_complete(coros_or_future)
-
-    def arungather(self, *coros_or_futures, return_exceptions=False):
-        return self.ruc(
-            asyncio.gather(
-                *coros_or_futures,
-                loop=self.loop,
-                return_exceptions=return_exceptions
-            )
-        )
-
-    def test_construct(self):
+class ChannelTest(aiounittest.AsyncTestCase):
+    async def test_construct(self):
         """
             Test that we can even construct a Channel
         """
-        channel = Channel(loop=self.loop)
+        channel = Channel(loop=asyncio.get_event_loop())
         self.assertEqual(channel.maxsize, 0)
         self.assertFalse(channel.full())
         self.assertTrue(channel.empty())
-        channel = Channel(1, loop=self.loop)
+        channel = Channel(1)
         self.assertEqual(channel.maxsize, 1)
-        channel = Channel(maxsize=1, loop=self.loop)
+        channel = Channel(maxsize=1)
         self.assertEqual(channel.maxsize, 1)
 
-        self.assertRaises(TypeError, lambda: Channel([], loop=self.loop))
-        self.assertRaises(TypeError, lambda: Channel(1.0, loop=self.loop))
-        self.assertRaises(TypeError, lambda: Channel(-1, loop=self.loop))
+        self.assertRaises(TypeError, lambda: Channel([]))
+        self.assertRaises(TypeError, lambda: Channel(1.0))
+        self.assertRaises(TypeError, lambda: Channel(-1))
 
     def test_default_loop(self):
         new_loop = asyncio.new_event_loop()
@@ -42,269 +29,253 @@ class ChannelTest(TestCase):
         self.assertEqual(channel._loop, new_loop)
         new_loop.close()
 
-    def test_repr(self):
-        channel = Channel(loop=self.loop)
+    async def test_repr(self):
+        channel = Channel()
         self.assertEqual(repr(channel),
                          "<Channel at 0x{:02x} maxsize=0 qsize=0>".format(id(channel)))
 
-    def test_str(self):
-        channel = Channel(loop=self.loop)
+    async def test_str(self):
+        channel = Channel()
         self.assertEqual(str(channel),
                          "<Channel maxsize=0 qsize=0>")
 
-    def test_put_nowait_get_nowait(self):
-        channel = Channel(1, loop=self.loop)
+    async def test_put_nowait_get_nowait(self):
+        channel = Channel(1)
         channel.put_nowait("foo")
         self.assertRaises(ChannelFull, lambda: channel.put_nowait("bar"))
         self.assertEqual("foo", channel.get_nowait())
         self.assertRaises(ChannelEmpty, lambda: channel.get_nowait())
 
-    def test_put_get(self):
+    async def test_put_get(self):
         """
             Simple put/get test
         """
         testitem = {"foo": "bar"}
-        channel = Channel(1, loop=self.loop)
-        self.ruc(channel.put(testitem))
+        channel = Channel(1)
+        await channel.put(testitem)
 
         self.assertEqual(channel.qsize(), 1)
         self.assertTrue(channel.full())
         self.assertFalse(channel.empty())
 
-        item = self.ruc(channel.get())
+        item = await channel.get()
         self.assertEqual(item, testitem)
         self.assertEqual(channel.qsize(), 0)
         self.assertFalse(channel.full())
         self.assertTrue(channel.empty())
 
-    def test_fifo_ordering(self):
+    async def test_fifo_ordering(self):
         """
             Test that items maintain order
         """
-        channel = Channel(3, loop=self.loop)
+        channel = Channel(3)
         testitems = [
             "first", "second", "third"
         ]
 
-        @asyncio.coroutine
-        def add_items():
-            for item in testitems:
-                yield from channel.put(item)
-
-        @asyncio.coroutine
-        def get_items():
-            out = []
-            while not channel.empty():
-                item = yield from channel.get()
-                out.append(item)
-            return out
+        for item in testitems:
+            await channel.put(item)
 
         # add and check for full
-        self.ruc(add_items())
         self.assertTrue(channel.full())
         # retreive and check that everything matches
-        outitems = self.ruc(get_items())
+        outitems = []
+        while not channel.empty():
+            item = await channel.get()
+            outitems.append(item)
+        return outitems
+
         self.assertEqual(outitems, testitems)
 
-    def test_get_throws_channel_closed(self):
+    async def test_get_throws_channel_closed(self):
         """
             Test that even though a blocking .get() is pending
             on an empty queue, a close() to that queue will make
             the .get() throw a ChannelClosed error
         """
-        channel = Channel(1, loop=self.loop)
+        channel = Channel(1)
 
-        @asyncio.coroutine
-        def wait_close():
-            yield from asyncio.sleep(0.01, loop=self.loop)
+        async def wait_close():
+            await asyncio.sleep(0.01)
             channel.close()
 
-        (get_return, _) = self.rucgather(channel.get(), wait_close(), return_exceptions=True)
+        (get_return, _) = await asyncio.gather(channel.get(), wait_close(), return_exceptions=True)
         self.assertIsInstance(get_return, ChannelClosed)
 
-    def test_put_throws_channel_closed(self):
+    async def test_put_throws_channel_closed(self):
         """
             Test that when a put blocks, and a channel is closed, the
             put will throw a ChannelClosed instead of waiting to add to channel
         """
-        channel = Channel(1, loop=self.loop)
+        channel = Channel(1)
         channel.put_nowait("foo")
         self.assertTrue(channel.full())
 
-        @asyncio.coroutine
-        def wait_close():
-            yield from asyncio.sleep(0.01, loop=self.loop)
+        async def wait_close():
+            await asyncio.sleep(0.01)
             channel.close()
 
-        (put_return, _) = self.rucgather(channel.put("bar"), wait_close(), return_exceptions=True)
+        (put_return, _) = await asyncio.gather(
+            channel.put("bar"),
+            wait_close(),
+            return_exceptions=True
+        )
         self.assertIsInstance(put_return, ChannelClosed)
         self.assertTrue(channel.closed())
 
-    def test_multiple_blocking_gets(self):
+    async def test_multiple_blocking_gets(self):
         """
             Test that a channel with multiple running get() still works
             out fine when the channel is closed
         """
-        channel = Channel(1, loop=self.loop)
+        channel = Channel(1)
 
-        @asyncio.coroutine
-        def wait_close():
-            yield from asyncio.sleep(0.01, loop=self.loop)
+        async def wait_close():
+            await asyncio.sleep(0.01)
             channel.close()
 
         futures = [channel.get() for _ in range(100)]
         futures.insert(50, wait_close())
 
-        result = self.rucgather(*futures, return_exceptions=True)
+        result = await asyncio.gather(*futures, return_exceptions=True)
         result.pop(50)  # pop the result for wait_close()
         for res in result:
             self.assertIsInstance(res, ChannelClosed)
 
-    def test_multiple_blocking_puts(self):
+    async def test_multiple_blocking_puts(self):
         """
             Test that a channel with multiple running put() still works
             out fine when the channel is closed
         """
-        channel = Channel(1, loop=self.loop)
+        channel = Channel(1)
         channel.put_nowait("foo")
         self.assertTrue(channel.full())
 
-        @asyncio.coroutine
-        def wait_close():
-            yield from asyncio.sleep(0.01, loop=self.loop)
+        async def wait_close():
+            await asyncio.sleep(0.01)
             channel.close()
 
         futures = [channel.put(i) for i in range(100)]
         futures.insert(50, wait_close())
 
-        result = self.rucgather(*futures, return_exceptions=True)
+        result = await asyncio.gather(*futures, return_exceptions=True)
         result.pop(50)  # pop the result for wait_close()
         for res in result:
             self.assertIsInstance(res, ChannelClosed)
 
-    def test_join(self):
+    async def test_join(self):
         """
             Test that a channel is joinable (when closed, and queue empty)
         """
-        channel = Channel(1000, loop=self.loop)
+        channel = Channel(1000)
         [channel.put_nowait(i) for i in range(1000)]
         self.assertTrue(channel.full())
         # create 1000 gets, should complete the queue
         gets = [channel.get() for _ in range(1000)]
 
-        @asyncio.coroutine
-        def runner():
+        async def runner():
             # sleep a bit, then call 1000 gets on channel, calling channel.close() in the middle
-            yield from asyncio.sleep(0.01, loop=self.loop)
+            await asyncio.sleep(0.01)
             n = 0
             for c in gets:
                 n += 1
                 if n == 500:
                     channel.close()
-                yield from c
+                await c
 
-        @asyncio.coroutine
-        def test():
-            self.loop.create_task(runner())  # run the getters in the backgrund
-            yield from asyncio.wait_for(channel.join(), timeout=2, loop=self.loop)
+        async def test():
+            asyncio.ensure_future(runner())  # run the getters in the backgrund
+            await asyncio.wait_for(channel.join(), timeout=2)
 
-        self.ruc(test())
+        await test()
 
-    def test_put_when_closed(self):
-        channel = Channel(1, loop=self.loop)
+    async def test_put_when_closed(self):
+        channel = Channel(1)
         channel.close()
-        self.assertRaises(ChannelClosed, lambda: self.ruc(channel.put("foo")))
+        with self.assertRaises(ChannelClosed):
+            await channel.put("foo")
 
-    def test_double_close(self):
-        channel = Channel(1, loop=self.loop)
+    async def test_double_close(self):
+        channel = Channel(1)
         self.assertFalse(channel.closed())
         channel.close()
         self.assertTrue(channel.closed())
         channel.close()
         self.assertTrue(channel.closed())
 
-    def test_putter_cancel(self):
-        channel = Channel(1, loop=self.loop)
-        self.ruc(channel.put("foo"))
+    async def test_putter_cancel(self):
+        channel = Channel(1)
+        await channel.put("foo")
         # next put will block as channel is full
         self.assertTrue(channel.full())
 
-        @asyncio.coroutine
-        def test_put():
-            yield from channel.put("bar")
+        async def test_put():
+            await channel.put("bar")
 
-        @asyncio.coroutine
-        def test_cancel():
-            yield from asyncio.sleep(0.01, loop=self.loop)
+        async def test_cancel():
+            await asyncio.sleep(0.01)
             channel._putters[0].cancel()
 
-        result = self.rucgather(test_put(), test_cancel(), return_exceptions=True)
+        result = await asyncio.gather(test_put(), test_cancel(), return_exceptions=True)
         self.assertIsInstance(result[0], asyncio.CancelledError)
 
-    def test_putter_exception(self):
-        channel = Channel(1, loop=self.loop)
-        self.ruc(channel.put("foo"))
+    async def test_putter_exception(self):
+        channel = Channel(1)
+        await channel.put("foo")
         # next put will block as channel is full
         self.assertTrue(channel.full())
 
-        @asyncio.coroutine
-        def test_put():
-            yield from channel.put("bar")
+        async def test_put():
+            await channel.put("bar")
 
-        @asyncio.coroutine
-        def test_cancel():
-            yield from asyncio.sleep(0.01, loop=self.loop)
+        async def test_cancel():
+            await asyncio.sleep(0.01)
             channel._maxsize = 2  # For hitting a different code branch in Channel
             channel._putters[0].set_exception(TypeError('random type error'))
 
-        result = self.rucgather(test_put(), test_cancel(), return_exceptions=True)
+        result = await asyncio.gather(test_put(), test_cancel(), return_exceptions=True)
         self.assertIsInstance(result[0], TypeError)
 
-    def test_getter_cancel(self):
-        channel = Channel(1, loop=self.loop)
+    async def test_getter_cancel(self):
+        channel = Channel(1)
 
-        @asyncio.coroutine
-        def test_get():
-            yield from channel.get()
+        async def test_get():
+            await channel.get()
 
-        @asyncio.coroutine
-        def test_cancel():
-            yield from asyncio.sleep(0.01, loop=self.loop)
+        async def test_cancel():
+            await asyncio.sleep(0.01)
             channel._getters[0].cancel()
 
-        result = self.rucgather(test_get(), test_cancel(), return_exceptions=True)
+        result = await asyncio.gather(test_get(), test_cancel(), return_exceptions=True)
         self.assertIsInstance(result[0], asyncio.CancelledError)
 
-    def test_getter_exception(self):
-        channel = Channel(1, loop=self.loop)
+    async def test_getter_exception(self):
+        channel = Channel(1)
 
-        @asyncio.coroutine
-        def test_get():
-            yield from channel.get()
+        async def test_get():
+            await channel.get()
 
-        @asyncio.coroutine
-        def test_cancel():
-            yield from asyncio.sleep(0.01, loop=self.loop)
+        async def test_cancel():
+            await asyncio.sleep(0.01)
             channel.empty = lambda: False  # For hitting a different code branch in Channel
             channel._getters[0].set_exception(TypeError('random type error'))
 
-        result = self.rucgather(test_get(), test_cancel(), return_exceptions=True)
+        result = await asyncio.gather(test_get(), test_cancel(), return_exceptions=True)
 
         self.assertIsInstance(result[0], TypeError)
 
-    def test_getter_already_done(self):
-        channel = Channel(2, loop=self.loop)
+    async def test_getter_already_done(self):
+        channel = Channel(2)
 
-        @asyncio.coroutine
-        def test_done_first_then_put():
-            yield from asyncio.sleep(0.01, loop=self.loop)
+        async def test_done_first_then_put():
+            await asyncio.sleep(0.01)
             channel.put_nowait("foo")
             channel.put_nowait("foo")
 
-        self.rucgather(channel.get(), channel.get(), test_done_first_then_put())
+        await asyncio.gather(channel.get(), channel.get(), test_done_first_then_put())
 
-    def test_get_nowait_raises_closed(self):
-        channel = Channel(1, loop=self.loop)
+    async def test_get_nowait_raises_closed(self):
+        channel = Channel(1)
         channel.put_nowait("foo")
         channel.close()
 
@@ -313,7 +284,7 @@ class ChannelTest(TestCase):
 
         self.assertRaises(ChannelClosed, lambda: channel.get_nowait())
 
-    def test_iter(self):
-        channel = Channel(loop=self.loop)
+    async def test_iter(self):
+        channel = Channel()
         [channel.put_nowait(n) for n in range(5)]
         self.assertEqual(list(range(5)), list(channel))
