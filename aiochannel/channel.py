9eae29ae0e9b683be301c615781aa817077a8659
Dmitry Orlov <me@mosquito.su>
2023-02-17 15:35:05 +0300

    Add typing

--- aiochannel/channel.py
+++ aiochannel/channel.py
@@ -1,6 +1,10 @@
 from .errors import ChannelClosed, ChannelFull, ChannelEmpty
 from collections import deque
-from asyncio import Event, Future, get_event_loop
+from asyncio import AbstractEventLoop, Event, Future, get_event_loop
+from typing import Any, Deque, Generic, Iterator, TypeVar
+
+
+T = TypeVar("T", bound=Any)
 
 
 #
@@ -8,12 +12,20 @@ from asyncio import Event, Future, get_event_loop
 # The first Channel implementation simply wrapped a closed Event and a Queue, and exposed
 # The same-ish API as Queue, but having access to the internals is way easier to deal with.
 #
-class Channel(object):
+class Channel(Generic[T]):
     """
         A Channel is a closable queue. A Channel is considered "finished" when
         it is closed and drained (unlike a queue which is "finished" when the queue
         is empty)
     """
+
+    _getters: Deque[Future]
+    _putters: Deque[Future]
+    _maxsize: int
+    _loop: AbstractEventLoop
+    _finished: Event
+    _close: Event
+    _queue: Deque[T]
 
     def __init__(self, maxsize=0, *, loop=None):
         if loop is None:
@@ -35,16 +47,16 @@ class Channel(object):
 
         self._init()
 
-    def _init(self):
+    def _init(self) -> None:
         self._queue = deque()
 
-    def _get(self):
+    def _get(self) -> T:
         return self._queue.popleft()
 
-    def _put(self, item):
+    def _put(self, item: T) -> None:
         self._queue.append(item)
 
-    def _wakeup_next(self, waiters):
+    def _wakeup_next(self, waiters: Deque[Future]) -> None:
         # Wake up the next waiter (if any) that isn't cancelled.
         while waiters:
             waiter = waiters.popleft()
@@ -52,28 +64,28 @@ class Channel(object):
                 waiter.set_result(None)
                 break
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return '<{} at {:#x} maxsize={!r} qsize={!r}>'.format(
             type(self).__name__, id(self), self._maxsize, self.qsize())
 
-    def __str__(self):
+    def __str__(self) -> str:
         return '<{} maxsize={!r} qsize={!r}>'.format(
             type(self).__name__, self._maxsize, self.qsize())
 
-    def qsize(self):
+    def qsize(self) -> int:
         """Number of items in the channel buffer."""
         return len(self._queue)
 
     @property
-    def maxsize(self):
+    def maxsize(self) -> int:
         """Number of items allowed in the channel buffer."""
         return self._maxsize
 
-    def empty(self):
+    def empty(self) -> bool:
         """Return True if the channel is empty, False otherwise."""
         return not self._queue
 
-    def full(self):
+    def full(self) -> bool:
         """Return True if there are maxsize items in the channel.
         Note: if the Channel was initialized with maxsize=0 (the default),
         then full() is never True.
@@ -83,7 +95,7 @@ class Channel(object):
         else:
             return self.qsize() >= self._maxsize
 
-    async def put(self, item):
+    async def put(self, item: T) -> None:
         """Put an item into the channel.
         If the channel is full, wait until a free
         slot is available before adding item.
@@ -91,7 +103,7 @@ class Channel(object):
         This method is a coroutine.
         """
         while self.full() and not self._close.is_set():
-            putter = Future(loop=self._loop)
+            putter: Future = self._loop.create_future()
             self._putters.append(putter)
             try:
                 await putter
@@ -106,7 +118,7 @@ class Channel(object):
                 raise
         return self.put_nowait(item)
 
-    def put_nowait(self, item):
+    def put_nowait(self, item: T) -> None:
         """Put an item into the channel without blocking.
         If no free slot is immediately available, raise ChannelFull.
         """
@@ -117,13 +129,13 @@ class Channel(object):
         self._put(item)
         self._wakeup_next(self._getters)
 
-    async def get(self):
+    async def get(self) -> T:
         """Remove and return an item from the channel.
         If channel is empty, wait until an item is available.
         This method is a coroutine.
         """
         while self.empty() and not self._close.is_set():
-            getter = Future(loop=self._loop)
+            getter: Future = self._loop.create_future()
             self._getters.append(getter)
             try:
                 await getter
@@ -138,7 +150,7 @@ class Channel(object):
                 raise
         return self.get_nowait()
 
-    def get_nowait(self):
+    def get_nowait(self) -> T:
         """Remove and return an item from the channel.
         Return an item if one is immediately available, else raise ChannelEmpty.
         """
@@ -155,12 +167,12 @@ class Channel(object):
         self._wakeup_next(self._putters)
         return item
 
-    async def join(self):
+    async def join(self) -> None:
         """Block until channel is closed and channel is drained
         """
         await self._finished.wait()
 
-    def close(self):
+    def close(self) -> None:
         """Marks the channel is closed and throw a ChannelClosed in all pending putters"""
         self._close.set()
         # cancel putters
@@ -177,15 +189,15 @@ class Channel(object):
             # already empty, mark as finished
             self._finished.set()
 
-    def closed(self):
+    def closed(self) -> bool:
         """Returns True if the Channel is marked as closed"""
         return self._close.is_set()
 
-    def __aiter__(self):
+    def __aiter__(self) -> "Channel":
         """Returns an async iterator (self)"""
         return self
 
-    async def __anext__(self):
+    async def __anext__(self) -> T:
         try:
             data = await self.get()
         except ChannelClosed:
@@ -193,5 +205,5 @@ class Channel(object):
         else:
             return data
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[T]:
         return iter(self._queue)
